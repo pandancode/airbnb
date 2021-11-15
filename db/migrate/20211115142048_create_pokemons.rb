843d9a9095193469c72ca68d995a8759a0aed492
Yilun Chen <yilun.chen@web.de>
2021-11-15 14:21:01 +0000

    Added pokemon model

---
+++ db/migrate/20211115142048_create_pokemons.rb
@@ -0,0 +1,12 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :pokemon_name
+      t.integer :level
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
