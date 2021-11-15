b925c1aa2af3ce34c33a06eb4c8572633e69ca76
Yilun Chen <yilun.chen@web.de>
2021-11-15 11:48:13 +0000

    Added comment into initializer, don't think we need to fully set this up now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,6 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  # Yilun: I don't think I will need to change it right now
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
