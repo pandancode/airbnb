bae2c7e83b8abb76710d0f9c4d43ea77e7c04a0e
Sarah McElligott <sarahmce@protonmail.com>
2021-11-15 16:32:26 +0000

    create method in pokemon controller created

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,16 @@
 class PokemonsController < ApplicationController
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.save
+      redirect_to pokemon_path(@pokemon)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:level, :price, :descriptiom)
+  end
 end
