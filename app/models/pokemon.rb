4beffc9ffe0185d6ac675b9bc32803777187a024
Amin Mallol <aminm-34@hotmail.fr>
2021-11-15 17:35:20 +0000

    Implementing the seed, need to pull too

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,7 +6,7 @@ class Pokemon < ApplicationRecord
   validates :level, presence: true, inclusion: { in: 1..100 }
   validates :level, numericality: { only_integer: true }
   validates :price, presence: true
-  validates :price, greater_than: 0
+  # validates :price, greater_than: 0
   validates :price, numericality: true
   # validates :description, presence: true
 end
