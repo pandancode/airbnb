7fd8a489be61f0b746380d71fe3fcd750bff8646
Yilun Chen <yilun.chen@web.de>
2021-11-15 14:39:41 +0000

    Added validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :pokemon
   belongs_to :user
+
+  validates :price, presence: true
+  validates :price, greater_than: 0
+  validates :price, numericality: true
 end
--- db/migrate/20211115142248_create_transactions.rb
+++ db/migrate/20211115142248_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
       t.float :price
-      t.boolean :completed
+      t.boolean :completed, default: false
       t.references :pokemon, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
