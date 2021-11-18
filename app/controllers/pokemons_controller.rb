cbcb695418ab02042e77aef17cf7c5857b3b7b29
Giorgio Gristina <giorgio.gristina91@gmail.com>
2021-11-18 17:27:19 +0000

    completed update edit btn

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -26,12 +26,26 @@ class PokemonsController < ApplicationController
     end
   end
 
+  def edit
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])   
+    if @pokemon.user == current_user
+      @pokemon.update(pokemon_params)
+    else
+      flash.alert = "You do not own the pokemon. however you can not edit this action"
+    end 
+    redirect_to pokemon_path(@pokemon)
+  end
+
   def destroy
     @pokemon = Pokemon.find(params[:id])
     if @pokemon.user == current_user
       @pokemon.destroy
     else
-      flash.alert = "You do not own the pokemon. howeever you can not perform this action"
+      flash.alert = "You do not own the pokemon. however you can not delete this action"
     end 
     redirect_to pokemons_path
   end
