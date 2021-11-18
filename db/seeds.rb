16ab483e7ec63219b31c8e75154da23593266b58
Sarah McElligott <sarahmce@protonmail.com>
2021-11-18 10:46:06 +0000

    geocoder gem added

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,11 @@
 # Seeding users, SO guide and also documentation
 # https://stackoverflow.com/questions/12418584/seeding-users-with-devise-in-ruby-on-rails
 # https://guides.rubyonrails.org/security.html
-<<<<<<< HEAD
-counter = 1
-
-puts "Cleaning user database"
-User.destroy_all
-puts "Cleaning user database: done"
-=======
 # counter = 1
 
 # puts "Cleaning user database"
 # User.destroy_all
 # puts "Cleaning user database: done"
->>>>>>> 457ad06b0d1a228f5c563b61d275f21fa6e355c1
 
 # 3.times do
 #   puts "Creating user #{counter}"
@@ -27,21 +19,6 @@ puts "Cleaning user database: done"
 #   new_user.first_name = "John#{counter}"
 #   new_user.last_name = "Doe#{counter}"
 #   new_user.phone_number = "0123467800#{counter}"
-<<<<<<< HEAD
-
-#   puts new_user.email
-#   puts new_user.encrypted_password
-#   puts "Is the user valid? #{new_user.valid?}"
-#   new_user.save!
-#   puts "Done with creating user #{counter}"
-#   puts ""
-#   counter += 1
-# end
-
-100.times do
-  puts "Start"
-  puts "Creating pokemon"
-=======
 
 #   puts new_user.email
 #   puts new_user.encrypted_password
@@ -60,16 +37,11 @@ puts "Done destroying all pokemon"
   counter = 1
   puts "Start creating pokemon #{counter}"
   desc = ["My favorite", "My first catch", "Caught in London", "Caught in Paris", "Caught in Tokyo", "Caught in 2018", "Shiny available upon request"]
->>>>>>> 457ad06b0d1a228f5c563b61d275f21fa6e355c1
 
   Pokemon.create(
     level: 100,
     price: 10,
-<<<<<<< HEAD
-    description: "beautiful",
-=======
     description: desc.sample,
->>>>>>> 457ad06b0d1a228f5c563b61d275f21fa6e355c1
     pokemon_name: PokeApi.get(pokemon: (1..898).to_a.sample).name,
     user_id: (1..20).to_a.sample
   )
