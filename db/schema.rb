669218ebdfa933fcaaebd69e164bc66139809d18
Sarah McElligott <sarahmce@protonmail.com>
2021-11-18 11:07:30 +0000

    longitude and latitiude added to pokemon.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_163134) do
+ActiveRecord::Schema.define(version: 2021_11_18_105828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_163134) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "sold", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pokemons_on_user_id"
   end
 
