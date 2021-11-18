e128b19a1869b830a677c3970f5b5bf44015c945
Yilun Chen <yilun.chen@web.de>
2021-11-18 16:41:58 +0000

    interim changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "transactions", to: "transactions#index"
+  get "search", to: "pokemons#search", alias: :search
   resources :pokemons, only: [:index, :show, :new, :create, :destroy] do
     resources :transactions, only: [:new, :create]
   end
